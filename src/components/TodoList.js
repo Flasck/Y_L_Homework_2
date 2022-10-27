b5e2e219f01056d73f73f30c42c8431a3fbdf3be
Flasck <po_6706@mail.ru>
2022-10-27 19:16:28 +0300

    try 2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -22,7 +22,7 @@ export function TodoList() {
     <div>
       <div>
         <input data-testid="input-add" value={text} onChange={onChange} />
-        <button data-testid="button-add" onClick={onClick}>Добавить</button>
+        <button data-testid="button-add" onClick={onClick} type="button">Добавить</button>
       </div>
       <div data-testid="list" className="list">
         {lodash.map(items, (text, i) => <TodoItem key={i} index={i} />)}
