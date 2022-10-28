10852d22bf02c6fd720269c26c55f1651e350e15
SergeiKachenia <serezha100500@mail.ru>
2022-10-28 14:18:28 +0300

    pr check 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,8 @@ export function Home() {
       <h1 data-testid="page-title">Home</h1>
       <p>This is the list.</p>
       <h1> TEST PR</h1>
-      <h3> TEST PR2</h3>
+      <h1> TEST PR2</h1>
+      <h1> TEST PR3</h1>
       <TodoList />
     </>
   );
